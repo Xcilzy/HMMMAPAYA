2a9cc30555c2e8c227e116c5496b2b7582d1c805
Xcilzy <azkagithubb@gmail.com>
2025-05-10 13:26:14 +0800

    Bang?

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Perkenalkan", "Namanya", "Adalah", "DetchCilzy", "Dia",
+  "Tidak", "Pernah", "Menyerah", "Walaupun", "Sudah", "Sering", "Gagal", "Dalam", "Dunia", "Pemograman", "Terimakasih"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    backDelay: 1000,
+    loop: true
+});
